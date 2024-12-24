72f4eb282f5bee64611920514fa8e60bf915a416
Saheedatt <afolabisaheedat1@gmail.com>
2024-12-24 06:36:18 +0100

    Page layout structure

---
+++ src/components/Dashboard/Layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import SideNav from "./Sidebar";
+import Header from "./Header";
+import { PropsWithChildren } from "react";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="flex h-screen">
+      <SideNav isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+      <div className="flex-1 flex flex-col">
+        <Header toggleSidebar={toggleSidebar} />
+
+        <main className="p-4 flex-1 bg-white">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
