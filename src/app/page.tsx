8e083ea714b2b2a1f6c535693c29252484326c11
Saheedatt <afolabisaheedat1@gmail.com>
2024-12-24 06:48:06 +0100

    Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-"use client"
-import Navigation from "@/components/Landing/Navbar";
-import ErrorBoundary from "@/components/ErrorBoundary";
+"use client";
+
+import LandingPage from "@/components/Landing/Home";
 
 export default function Home() {
   return (
-    <ErrorBoundary>
-      <div>
-        <Navigation />
-        <main id="main-content" className="flex flex-col justify-center items-center">
-          <h1 className="text-3xl font-bold">This is Foyer</h1>
-          <p className="text-lg">It is a safe space</p>
-        </main>
-      </div>
-    </ErrorBoundary>
+      <LandingPage />
   );
 }
