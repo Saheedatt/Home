a76148d6dd4ad9f41a9d127b6b340a511ccce32e
Saheedatt <afolabisaheedat1@gmail.com>
2025-03-12 17:34:50 +0100

    Added session provider tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,46 @@
-import type { Metadata } from "next";
+"use client"
+// import type { Metadata } from "next";
+// import "./globals.css";
+
+// import { ThemeProvider } from "@/contexts/ThemeContext";
+// import { SessionProvider } from "next-auth/react";
+
+// export const metadata: Metadata = {
+//   title: "Foyer",
+//   description: "Make your home smart. Control everything at your fingertips",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <SessionProvider>
+//           <ThemeProvider>{children}</ThemeProvider>
+//         </SessionProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
+
+
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 import "./globals.css";
 
-import { ThemeProvider } from "@/contexts/ThemeContext";
-
-export const metadata: Metadata = {
-  title: "Foyer",
-  description: "Make your home smart. Control everything at your fingertips",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <SessionProvider>
         <ThemeProvider>{children}</ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
